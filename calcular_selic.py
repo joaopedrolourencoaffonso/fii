2d30af1dbe8df8f5c80709201424b22523b47692
joaopedrolourencoaffonso <56265673+joaopedrolourencoaffonso@users.noreply.github.com>
2020-02-17 19:30:40 -0300

    Update calcular_selic.py

--- calcular_selic.py
+++ calcular_selic.py
@@ -6,7 +6,6 @@ res = requests.get("https://fiis.com.br/lista-de-fundos-imobiliarios/")
 res.raise_for_status()
 obj = bs4.BeautifulSoup(res.text, features="html.parser")
 objeto = obj.select('.ticker')
-#print(objeto)
 target = "Cotação atual de"
 target1 = "."
 
@@ -24,14 +23,11 @@ for item in objeto:
                 temp4 = str(objeto1[i])
                 pos = temp4.find(target)
                 if pos > 0:
-                        #print(temp1[0])
                         nome = temp1[0]
-                        #print(str(objeto1[i-2]))
                         preco = str(objeto1[i-2])
                         preco = preco.split('>')
                         preco = preco[1].split('<')
                         preco = preco[0]
-                        #print("--------")
 
         ###########################################
         objeto1 = obj.select("#informations--indexes")
@@ -56,14 +52,14 @@ for item in objeto:
         pos = valor_patr.find(".")
         #print(pos)
         if pos > 0:
-                temp1 = valor_patr.split(".")           #usar prints para descobrir o que está errado
+                temp1 = valor_patr.split(".")           
                 temp1 = temp1[0] + "" + temp1[1]
                 temp1 = temp1.split(",")
                 temp1 = temp1[0] + "." + temp1[1]       #calculo de valor float de valor_patr
                 temp1 = float(temp1)
         else:
                 temp1 = valor_patr.split(",")
-                temp1 = temp1[0] + "." + temp1[1]       #calculo de valor float de valor_patr  #dessa maneira funciona
+                temp1 = temp1[0] + "." + temp1[1]       #calculo de valor float de valor_patr
                 temp1 = float(temp1)
         #
         pos = preco.find(".")
@@ -76,7 +72,7 @@ for item in objeto:
                 temp2 = float(temp2)
         else:
                 temp2 = preco.split(",")
-                temp2 = temp2[0] + "." + temp2[1]       #calculo de valor float de preco #dessa maneira funciona
+                temp2 = temp2[0] + "." + temp2[1]       #calculo de valor float de preco
                 temp2 = float(temp2)
         #
         pvp = temp2 / temp1
@@ -86,15 +82,12 @@ for item in objeto:
         print("Nome-------Preço-------Dividendo Yield-------Último Rendimento-------Patrimônio Líquido-------Valor Patrimonial-------p/vp")
         print(nome + "     " + preco + "         " + yield_value + "                    " + ultimo_rend + "                  " + patrimonio + "                    " + valor_patr + "               " + pvp)
         print("--------------------------------------------------------------------------------------------------------------------------")
+            
 
-        #break
-                
-
-        
-
-#O QUE FALTA FAZER:
-        #4) Calcular p/vp --> usar prints para descobrir o que está dando errado
-
-
-#NOTA: Quando você aplicar banco de dados, uma boa ideia seria colocar uma opção de olhar se há fundos novos ou não.
+#NOTA: Como você pode ver, esse script já é capaz de coletar informações referentes ao dividendo yield, preço, patrimônio, etc
+#Funciona perfeitamente, mas, ainda há dois problemas:
+#       1) A apresentação ainda é problemática devido ao fato de que o tamanho das variáveis muda muito
+#       2) Demora de 6 a 8 minutos para terminar de rodar, comparado a
+#          analisar todos os fundos 1 a 1 ainda é muito bom, mas sinto que pode melhorar usando processos em paralelo, 
+#          vamos ver o que o futuro nos reserva :)
 
